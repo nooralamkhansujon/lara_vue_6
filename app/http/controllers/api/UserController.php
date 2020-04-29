d5834820dfac0b54c2f2a06c9f94acbdca10fb0d
Nooralam khan <CoreyMShafer@gmail.com>
2020-04-29 23:42:16 +0600

    UserController controller added and login function added

---
+++ app/http/controllers/api/UserController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+use Auth;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserController extends Controller
+{
+
+    public function login(Request $request)
+    {
+        $username = $request->email;
+        $password = bcrypt($request->password);
+        $user     = User::where('email',$username)
+                    ->where('password',$password)->first();
+        $token    = Hash::make($request->password);
+        $user->api_token = $token;
+        $user->save();
+        return $token;
+
+        // if($user)
+        // {
+
+        // }
+        // else{
+
+        // }
+    }
+}
