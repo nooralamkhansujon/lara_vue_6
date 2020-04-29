e4d3fabaea4f240d5f250087498f01585f239856
Nooralam khan <CoreyMShafer@gmail.com>
2020-04-29 23:39:12 +0600

    vue component and other resource added

---
+++ resources/js/components/LoginComponent.vue
@@ -0,0 +1,62 @@
+<template>
+    <v-app id="inspire">
+        <v-content>
+            <v-container class="fill-height" fluid>
+                <v-row align="center" justify="center">
+                    <v-col cols="12" sm="8" md="4">
+                        <v-card class="elevation-12">
+                            <v-toolbar color="error" dark flat>
+                                <v-toolbar-title>Login form</v-toolbar-title>
+                                <v-spacer />
+
+
+                            </v-toolbar>
+                            <v-card-text>
+                                <v-form>
+                                    <v-text-field
+                                        label="Login"
+                                        name="login"
+                                        prepend-icon="mdi-account-circle-outline"
+                                        type="email"
+                                        v-model="email"
+                                    />
+
+                                    <v-text-field
+                                        id="password"
+                                        label="Password"
+                                        name="password"
+                                        prepend-icon="mdi-lock"
+                                        type="password"
+                                        v-model="password"
+                                    />
+                                </v-form>
+                            </v-card-text>
+                            <v-card-actions>
+                                <v-spacer />
+                                <v-btn color="error" @click="login()">Login</v-btn>
+                            </v-card-actions>
+                        </v-card>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-content>
+    </v-app>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            email:'',
+            password:''
+        }
+    },
+    methods:{
+        login(){
+            localStorage.setItem('token','nooralamkhan25760300');
+        }
+    }
+};
+</script>
+
+<style lang="stylus" scoped></style>
---
+++ resources/js/router.js
@@ -0,0 +1,38 @@
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+import LoginComponent from './components/LoginComponent';
+import AdminComponent from './components/AdminComponent';
+
+Vue.use(VueRouter);
+
+
+const routes = [
+  {
+    path: '/',
+    redirect:'/login'
+  },
+  {
+    path:'/login',
+    component:LoginComponent,
+    name:'Login'
+  },
+  {
+    path:'/admin',
+    component:AdminComponent,
+    name:"Admin",
+    beforeEnter: (to, from, next) => {
+        if(localStorage.getItem('token'))
+        {
+            next();
+        }
+        else{
+            next('/login');
+        }
+
+    }
+  }
+
+];
+
+
+export default new VueRouter({routes});
